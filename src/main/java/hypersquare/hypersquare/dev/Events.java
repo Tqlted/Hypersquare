30a75584e938ea1bdf5b183c2307244e71d1fa2b
Tqlted <78366008+Tqlted@users.noreply.github.com>
2024-04-15 20:14:10 +0200

    feat: new events (#73)
        Player Respawn Event
        Player Take Damage Event
        Player Damage Player Event
        Player Damage Entity Event
        Entity Damage Player Event

--- src/main/java/hypersquare/hypersquare/dev/Events.java
+++ src/main/java/hypersquare/hypersquare/dev/Events.java
@@ -45,7 +45,12 @@ public enum Events implements Event {
     PLAYER_DROP_ITEM_EVENT(new PlayerDropItemEvent()),
     PLAYER_BREAK_ITEM_EVENT(new PlayerBreakItemEvent()),
     PLAYER_DAMAGE_ITEM_EVENT(new PlayerItemDamageEvent()),
-    PLAYER_MEND_ITEM_EVENT(new PlayerMendItemEvent())
+    PLAYER_MEND_ITEM_EVENT(new PlayerMendItemEvent()),
+    PLAYER_RESPAWN_EVENT(new PlayerRespawnEvent()),
+    PLAYER_TAKE_DAMAGE_EVENT(new PlayerTakeDamageEvent()),
+    PLAYER_DAMAGE_PLAYER_EVENT(new PlayerDamagePlayerEvent()),
+    PLAYER_DAMAGE_ENTITY_EVENT(new PlayerDamageEntityEvent()),
+    ENTITY_DAMAGE_PLAYER_EVENT(new EntityDamagePlayerEvent())
 
     ;
 
