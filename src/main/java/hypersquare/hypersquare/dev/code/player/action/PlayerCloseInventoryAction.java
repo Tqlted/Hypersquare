44cdcdb8f190aedff43f8a812365ab994342cfab
RedVortex <58099979+RedVortexDev@users.noreply.github.com>
2024-04-18 12:35:29 +0300

    feat: close inv action

---
+++ src/main/java/hypersquare/hypersquare/dev/code/player/action/PlayerCloseInventoryAction.java
@@ -0,0 +1,84 @@
+package hypersquare.hypersquare.dev.code.player.action;
+
+import hypersquare.hypersquare.dev.BarrelParameter;
+import hypersquare.hypersquare.dev.BarrelTag;
+import hypersquare.hypersquare.dev.action.Action;
+import hypersquare.hypersquare.dev.codefile.data.CodeActionData;
+import hypersquare.hypersquare.item.action.ActionItem;
+import hypersquare.hypersquare.item.action.ActionMenuItem;
+import hypersquare.hypersquare.item.action.player.PlayerActionItems;
+import hypersquare.hypersquare.item.value.DisplayValue;
+import hypersquare.hypersquare.menu.barrel.BarrelMenu;
+import hypersquare.hypersquare.play.CodeSelection;
+import hypersquare.hypersquare.play.execution.ExecutionContext;
+import hypersquare.hypersquare.util.color.Colors;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class PlayerCloseInventoryAction implements Action {
+    @Override
+    public void execute(@NotNull ExecutionContext ctx, @NotNull CodeSelection targetSel) {
+        for (Player p : targetSel.players()) {
+            p.closeInventory();
+        }
+    }
+
+    @Override
+    public BarrelParameter[] parameters() {
+        return new BarrelParameter[]{};
+    }
+
+    @Override
+    public BarrelTag[] tags() {
+        return new BarrelTag[]{};
+    }
+
+    @Override
+    public String getId() {
+        return "close_inventory";
+    }
+
+    @Override
+    public String getCodeblockId() {
+        return "player_action";
+    }
+
+    @Override
+    public String getSignName() {
+        return "CloseInv";
+    }
+
+    @Override
+    public String getName() {
+        return "Close Inventory";
+    }
+
+    @Override
+    public ActionMenuItem getCategory() {
+        return PlayerActionItems.INVENTORY_MENUS_CATEGORY;
+    }
+
+    @Override
+    public ItemStack item() {
+        return new ActionItem()
+            .setMaterial(Material.FIREWORK_ROCKET)
+            .setName(Component.text("Close Inventory").color(Colors.RED_LIGHT))
+            .setDescription(Component.text("Closes a player's currently"), Component.text("open inventory menu."))
+            .addAdditionalInfo(Component.text("This includes chat, game menu, etc."))
+            .setParameters(parameters())
+            .setTagAmount(tags().length)
+            .build();
+    }
+
+    @Override
+    public BarrelMenu actionMenu(CodeActionData data) {
+        return new BarrelMenu(this, 1, data)
+            .parameter("item", 4);
+    }
+}
